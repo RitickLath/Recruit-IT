87b6d5f3a04f0ff0d22b9bc6a1516c7d237c60cf
RitickLath <ritick943lath@gmail.com>
2025-03-09 21:15:16 +0530

    Added Transcation for Data Consistency while Registration

--- Backend/src/routes/Auth.route.ts
+++ Backend/src/routes/Auth.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { Admin, Applicant, Credentials, HR } from "../models";
 import bcryptjs from "bcryptjs";
+import mongoose from "mongoose";
 const AuthRouter = express.Router();
 
 // full name, email, password, role ->
@@ -13,39 +14,49 @@ AuthRouter.post("/register", async (req, res) => {
 
   // Will do Data Validation using Zod
 
+  // Init the session and start it
+  const session = await mongoose.startSession();
+  session.startTransaction();
+
   try {
     // Check if the user already exists
-    const isExists = await Credentials.findOne({ email });
+    const isExists = await Credentials.findOne({ email }).session(session);
     if (isExists) {
-      res
-        .status(400)
-        .json({ status: false, message: "User Already Exists. Please Login" });
-      return;
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(400).json({
+        status: false,
+        message: "User Already Exists. Please Login",
+      });
     }
 
     // Hash the password
     const hashedPassword = await bcryptjs.hash(password, 10);
 
     // Create user credentials
-    const user = await Credentials.create({
-      email,
-      password: hashedPassword,
-      fullName,
-      role,
-    });
+    const user = await Credentials.create(
+      [{ email, password: hashedPassword, fullName, role }],
+      { session }
+    );
 
     // Assign user to the correct model
     if (role === "admin") {
-      await Admin.create({ Credentials: user._id });
+      await Admin.create([{ Credentials: user[0]._id }], { session });
     } else if (role === "hr") {
-      await HR.create({ Credentials: user._id });
+      await HR.create([{ Credentials: user[0]._id }], { session });
     } else if (role === "applicant") {
-      await Applicant.create({ Credentials: user._id });
+      await Applicant.create([{ Credentials: user[0]._id }], { session });
     }
+
+    // COMMIT THE TRANSACTION
+    await session.commitTransaction();
+    session.endSession();
 
     res.json({ status: true, message: "User Registered Successfully." });
   } catch (e) {
     console.error(e);
+    await session.abortTransaction();
+    session.endSession();
     res.status(500).json({
       status: false,
       message: "Something went wrong while registering",
